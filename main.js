77b41a3f182ce74fd793ab3e33d378f5767915b9
kloied <franciscocaloia@hotmail.com>
2022-10-22 16:47:36 -0300

    Desafio entregable 2

--- main.js
+++ main.js
@@ -1,41 +1,83 @@
-class Container {
-  constructor() {
-    this.content = [];
-  }
+const fs = require("fs");
 
-  save(object) {
-    this.content.push(object);
+class Container {
+  constructor(path) {
+    this.path = path;
   }
-  getById(id) {
-    return this.content.find((element) => element.id === id);
+  async getAll() {
+    try {
+      return JSON.parse(await fs.promises.readFile(this.path, "utf-8"));
+    } catch (error) {
+      console.log(error);
+    }
   }
-  getAll() {
-    return this.content;
+  async save(object) {
+    try {
+      const content = await this.getAll();
+      content.push({
+        id: content.at(-1) ? content.at(-1).id + 1 : 1,
+        ...object,
+      });
+      await fs.promises.writeFile(this.path, JSON.stringify(content));
+      return content.at(-1).id;
+    } catch (error) {
+      console.log(error);
+    }
   }
-  deleteById(id) {
-    this.content.forEach((element, index) => {
-      if (element.id === id) this.content.splice(index, 1);
-    });
+  async getById(id) {
+    try {
+      const content = await this.getAll();
+      return content.find((element) => element.id === id);
+    } catch (error) {
+      console.log(error);
+    }
   }
-  deleteAll() {
-    this.content.splice(0, this.content.length);
+  async deleteById(id) {
+    try {
+      const content = await this.getAll();
+      content.forEach((element, index) => {
+        if (element.id === id) content.splice(index, 1);
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  async deleteAll() {
+    try {
+      await fs.promises.writeFile(this.path, "[]");
+    } catch (error) {
+      console.log(error);
+    }
   }
 }
 
-const containerPrueba = new Container();
+const containerPrueba = new Container("./info.txt");
 
-containerPrueba.save({ id: 1, name: "producto1", price: 2332 });
-containerPrueba.save({ id: 2, name: "producto2", price: 2332 });
-containerPrueba.save({ id: 3, name: "producto3", price: 2332 });
-containerPrueba.save({ id: 4, name: "producto4", price: 2332 });
-containerPrueba.save({ id: 5, name: "producto5", price: 2332 });
-console.log("\n\nTodos los productos guardados: ");
-console.log(containerPrueba.getAll());
-console.log("\n\nNombre del producto con ID = 3: ");
-console.log(containerPrueba.getById(3).name);
-containerPrueba.deleteById(4);
-console.log("\n\nContenedor luego de eliminar el producto con ID = 4");
-console.log(containerPrueba.getAll());
-containerPrueba.deleteAll();
-console.log("\n\nContenedor luego de eliminar todos los productos: ");
-console.log(containerPrueba.getAll());
+(async () => {
+  console.log(
+    "ID del producto guardado: " +
+      (await containerPrueba.save({ name: "producto1", price: 2332 }))
+  );
+  console.log(
+    "ID del producto guardado: " +
+      (await containerPrueba.save({ name: "producto2", price: 2332 }))
+  );
+  console.log(
+    "ID del producto guardado: " +
+      (await containerPrueba.save({ name: "producto3", price: 2332 }))
+  );
+  console.log(
+    "ID del producto guardado: " +
+      (await containerPrueba.save({ name: "producto4", price: 2332 }))
+  );
+  console.log("\n\nTodos los productos guardados: ");
+  console.log(await containerPrueba.getAll());
+  console.log("\n\nProducto con ID = 3: ");
+  console.log(await containerPrueba.getById(3));
+  console.log("\n\nContenedor luego de eliminar el producto con ID = 4");
+  await containerPrueba.deleteById(4);
+  console.log(await containerPrueba.getAll());
+  console.log("\n\nContenedor luego de eliminar todos los productos: ");
+  await containerPrueba.deleteAll();
+  console.log(await containerPrueba.getAll());
+})();
